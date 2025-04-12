7de251688ab28df4c0a9e20463a1132a93601143
gabonbon0528 <judy860528@gmail.com>
2025-04-12 20:57:47 +0800

    feat: 更新 MainLayout 組件，新增滾動條和固定側邊欄功能以改善佈局

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -9,7 +9,14 @@ export const MainLayout = (props: { children: React.ReactNode }) => {
   return (
     <div className={"flex min-h-screen max-w-screen"}>
       <Sidebar />
-      <VStack padding={4} alignItems={"flex-start"} gap={4} flex={1}>
+      <VStack
+        padding={4}
+        alignItems={"flex-start"}
+        gap={4}
+        flex={1}
+        overflowY={"auto"}
+        height={"100vh"}
+      >
         <BreadCrumbs />
         {props.children}
       </VStack>
@@ -23,6 +30,8 @@ export const Sidebar = () => {
       padding={2}
       gap={2}
       bg={"teal"}
+      position={"sticky"}
+      top={0}
       className={"text-white w-14 h-screen"}
     >
       <UserButton />
