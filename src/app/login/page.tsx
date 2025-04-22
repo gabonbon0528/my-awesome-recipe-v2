d50c4fd699f81d2bbd8463ac2828aca7917779a1
gabonbon <judy860528@gmail.com>
2025-04-22 18:29:03 +0800

    feat: add signup form with validation using Zod and integrate into login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import LoginButton from "@/components/login/LoginButton";
+import LoginForm from "@/components/login/LoginForm";
 import { Flex, Text } from "@chakra-ui/react";
 
 export default function LoginPage() {
   return (
     <Flex flexDirection={"column"} gap={4} width={"100%"}>
       <Text>LoginPage</Text>
+      <LoginForm />
       <LoginButton />
     </Flex>
   );
