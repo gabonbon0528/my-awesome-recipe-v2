78d0bcf7490e187260934b10e611906305459229
gabonbon <judy860528@gmail.com>
2025-04-24 15:51:48 +0800

    feat: update validation messages in Signup and Login form schemas to Chinese for improved localization

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,37 +1,23 @@
 import { z } from "zod";
 
 export const SignupFormSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: "Name must be at least 2 characters long." })
-    .trim(),
-  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  name: z.string().min(2, { message: "至少2個字" }).trim(),
+  email: z.string().email({ message: "請輸入有效的電子郵件" }).trim(),
   password: z
     .string()
-    .min(8, { message: "Be at least 8 characters long" })
-    .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
-    .regex(/[0-9]/, { message: "Contain at least one number." })
+    .min(8, { message: "至少8個字" })
+    .regex(/[a-zA-Z]/, { message: "至少包含一個字母" })
+    .regex(/[0-9]/, { message: "至少包含一個數字" })
     .regex(/[^a-zA-Z0-9]/, {
-      message: "Contain at least one special character.",
+      message: "至少包含一個特殊字元",
     })
     .trim(),
 });
 
 export const LoginFormSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  email: z.string().email({ message: "請輸入有效的電子郵件" }).trim(),
   password: z
     .string()
-    .min(8, { message: "Be at least 8 characters long" })
+    .min(8, { message: "至少8個字" })
     .trim(),
 });
-
-export type FormState =
-  | {
-      errors?: {
-        name?: string[];
-        email?: string[];
-        password?: string[];
-      };
-      message?: string;
-    }
-  | undefined;
