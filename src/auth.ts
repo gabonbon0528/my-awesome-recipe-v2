076e754c0a25e46b75b42e0f5b509c3e3f26437c
gabonbon <judy860528@gmail.com>
2025-05-02 18:30:04 +0800

    feat: enhance authentication logic by adding user session management and updating user button to display username

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { db } from "@/config/firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import bcrypt from "bcryptjs";
+
+type User = {
+  id: string;
+  email: string;
+  username: string;
+};
 
 export const {
   handlers: { GET, POST },
@@ -20,9 +23,9 @@ export const {
     Credentials({
       name: "credentials",
       credentials: {
-        email: { type: "email" },
-        username: { type: "text" },
-        id: { type: "text" },
+        email: { type: "email", label: "Email" },
+        username: { type: "username", label: "Username" },
+        id: { type: "id", label: "Id" },
       },
       authorize: async (credentials) => {
         try {
@@ -44,10 +47,29 @@ export const {
   ],
   callbacks: {
     async session({ session, token }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
+      // 將 token 中的使用者資料存入 session
+      if (token.user) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.username = token.username;
       }
       return session;
     },
+    async jwt({ token, user }) {
+      // 將所有使用者資料存入 token
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.username = user.username;
+      }
+      return token;
+    },
+
+    // async session({ session, token }) {
+    //   if (token.sub && session.user) {
+    //     session.user.id = token.sub;
+    //   }
+    //   return session;
+    // },
   },
 });
--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -3,10 +3,14 @@ import { Avatar } from "@chakra-ui/react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function UserButton() {
   const [pressTimer, setPressTimer] = useState<NodeJS.Timeout | null>(null);
   const router = useRouter();
+  const session = useSession();
+  const user = session?.data?.user as { username: string };
+  console.log("💗💗💗", user);
 
   const handleMouseDown = () => {
     const timer = setTimeout(() => {
@@ -39,7 +43,7 @@ export default function UserButton() {
 
   return (
     <Link href="/">
-      <Avatar.Root 
+      <Avatar.Root
         size={"md"}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
@@ -47,8 +51,8 @@ export default function UserButton() {
         onTouchStart={handleMouseDown}
         onTouchEnd={handleMouseUp}
       >
-        <Avatar.Fallback name="Segun Adebayo" />
-        <Avatar.Image src="https://bit.ly/sage-adebayo" />
+        <Avatar.Fallback name={user?.username ?? ""} />
+        <Avatar.Image src="https://image1.gamme.com.tw/news2/2017/31/14/rJeapKCXlKCZrKQ.jpg" />
       </Avatar.Root>
     </Link>
   );
