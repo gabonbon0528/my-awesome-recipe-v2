fe08c335e025a04d4855c1c9030c35cfbaf4db28
gabonbon <judy860528@gmail.com>
2025-05-02 09:35:45 +0800

    refactor: rename 'name' to 'username' in SignupFormSchema and comment out regex validations for password

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const SignupFormSchema = z.object({
-  name: z.string().min(2, { message: "至少2個字" }).trim(),
+  username: z.string().min(2, { message: "至少2個字" }).trim(),
   email: z.string().email({ message: "請輸入有效的電子郵件" }).trim(),
   password: z
     .string()
     .min(8, { message: "至少8個字" })
-    .regex(/[a-zA-Z]/, { message: "至少包含一個字母" })
-    .regex(/[0-9]/, { message: "至少包含一個數字" })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "至少包含一個特殊字元",
-    })
+    // .regex(/[a-zA-Z]/, { message: "至少包含一個字母" })
+    // .regex(/[0-9]/, { message: "至少包含一個數字" })
+    // .regex(/[^a-zA-Z0-9]/, {
+    //   message: "至少包含一個特殊字元",
+    // })
     .trim(),
 });
 
