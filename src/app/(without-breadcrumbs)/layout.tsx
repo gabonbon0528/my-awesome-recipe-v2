8c99bd692ef594d90924fe8c5edebb947f68cb5f
gabonbon <judy860528@gmail.com>
2025-05-13 19:33:04 +0800

    feat: refactor layout components to utilize MainLayout for consistent structure across pages

--- src/app/(without-breadcrumbs)/layout.tsx
+++ src/app/(without-breadcrumbs)/layout.tsx
@@ -1,13 +1,5 @@
-import { Sidebar } from "@/components/layout/MainLayout";
-import { Box } from "@chakra-ui/react";
+import { MainLayout } from "@/components/layout/MainLayout";
 
 export default function Layout(props: { children: React.ReactNode }) {
-  return (
-    <div className={"flex min-h-screen max-w-screen"}>
-      <Sidebar />
-      <Box p={4} flex={1} overflowY={"auto"} height={"100vh"}>
-        {props.children}
-      </Box>
-    </div>
-  );
+  return <MainLayout>{props.children}</MainLayout>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { MainLayout } from "@/components/layout/MainLayout";
 import { Box, Heading, Link, Text } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <Box >
+    <MainLayout>
       <Heading>Welcome to My Awesome Recipe</Heading>
       <Text>
         This app will help you organize your recipes, calculate the cost of
@@ -10,11 +11,11 @@ export default function Page() {
       </Text>
       <Text>
         Click{" "}
-        <Link href="/recipe" colorPalette="teal" fontSize="xl">
+        <Link href="/recipe/create" colorPalette="teal" fontSize="xl">
           here Σ੧(❛□❛✿)
         </Link>{" "}
         to create a new recipe!
       </Text>
-    </Box>
+    </MainLayout>
   );
 }
